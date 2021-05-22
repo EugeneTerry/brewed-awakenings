0222af740b093bd18575d7f7fd99ad12c1175e23
EugeneTerry <eugene.terry73@gmail.com>
2021-05-22 14:11:57 -0500

    5/22/2021 save

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -7,10 +7,10 @@ const orders = getOrders()
 
 
 // Function whose responsibility is to find the product for an order
-const findproduct = (order, allProducts) => {
+const findProduct = (order, products) => {
     let orderProduct = null
 
-    for (const product of allProducts) {
+    for (const product of products) {
         if (product.id === order.productId) {
             orderProduct = product
         }
@@ -20,10 +20,10 @@ const findproduct = (order, allProducts) => {
 }
 
 // Function whose responsibility is to find the employee for an order
-const findemployee = (order, allEmployees) => {
+const findEmployee = (order, employees) => {
     let orderEmployee = null
 
-    for (const employee in allEmployees) {
+    for (const employee of employees) {
         if (employee.id === order.employeeId) {
             orderEmployee = employee
         }
@@ -33,14 +33,13 @@ const findemployee = (order, allEmployees) => {
 }
 
 export const Orders = () => {
-    let html = ""
-    html = "<ul>"
+    let html = "<ul>"
 
-    for (const order of orders) {
-        const employee = findEmployee(order, employees)
-        const product = findProduct(order)
+    for (const currentorder of orders) {
+        const currentemployee = findEmployee(currentorder, employees)
+        const currentproduct = findProduct(currentorder, products)
 
-        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+        html += `<li>${currentproduct.name} was sold by ${currentemployee.name} on ${new Date(currentorder.timestamp).toLocaleDateString()}</li>`
     }
 
     html += "</ul>"
